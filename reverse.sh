6f7a36457c6568fffda327a892caa36a5a163663
AngithPanicker <81224065+AngithPanicker@users.noreply.github.com>
2024-03-14 15:24:37 +0530

    Add files via upload

--- reverse.sh
+++ reverse.sh
@@ -1,24 +1,28 @@
-read num
-reverse=$(echo "$num" | rev)
-if [ "$num" -eq "$reverse" ]; then
-echo "$num is same when reversed."
+echo "Enter a number: "
+read number
+
+reverse=0
+original=$number
+
+while [ $number -ne 0 ]
+do
+    remainder=$(( $number % 10 ))
+    reverse=$(( $reverse * 10 + $remainder ))
+    number=$(( $number / 10 ))
+done
+
+if [ $original -eq $reverse ]
+then
+    echo "$original is a same when reversed."
 else
-echo "$num is not same when reversed."
+    echo "$original is not same when reversed."
 fi
 
 
-
-
-
-
-
-					      output
-mlm@mlm-OptiPlex-3020:~/Desktop/angith/mca$ chmod +x reverse.sh
-mlm@mlm-OptiPlex-3020:~/Desktop/angith/mca$ ./reverse.sh
-253
-253 is not same when reversed.
-mlm@mlm-OptiPlex-3020:~/Desktop/angith/mca$ chmod +x reverse.sh
-mlm@mlm-OptiPlex-3020:~/Desktop/angith/mca$ ./reverse.sh
-171
-171 is same when reversed.
+                                                       output
+			mlm@mlm-Vostro-3902:~/Desktop/angith/mca$ chmod +x reverse.sh
+			mlm@mlm-Vostro-3902:~/Desktop/angith/mca$ ./reverse.sh
+			Enter a number: 
+			121
+			121 is a same when reversed.
 
